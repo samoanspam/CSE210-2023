9aa2863d277b65326381687570991ffca73acfe4
samoanspam <thalpatane@byui.edu>
2023-09-13 12:29:19 -0600

    prep 1, too easy

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Reflection;
+using System.Runtime.Intrinsics.Arm;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        string firstName;
+        string lastName;
+        Console.WriteLine("What is your first name? ");
+        firstName = Console.ReadLine();
+        
+        Console.WriteLine("What is your last name? ");
+        lastName = Console.ReadLine();
+
+        System.Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}. ");
     }
 }
