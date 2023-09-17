5b0238fb351a9f00e650272ee50979738e9e7b36
samoanspam <thalpatane@byui.edu>
2023-09-16 22:06:44 -0600

    ?

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,37 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        int x = 5;
+        int y = 4;
+        string name;
+
+        System.Console.WriteLine("What is your name? ");
+
+        name = Console.ReadLine();
+
+        if (name == "John")
+        {
+            System.Console.WriteLine("Great name! ");
+        } 
+        else
+        {
+            System.Console.WriteLine("That's a cool name! ");
+        }
+
+
+        if (x > y)
+        {
+            System.Console.WriteLine("greater than y");
+        }
+        if (x < y)
+        {
+            System.Console.WriteLine("greater than x");
+        }
+        else
+        {
+            System.Console.WriteLine("less than both");
+        }
     }
 }
