905842b561b094b815f0b7752484098397c45d69
samoanspam <thalpatane@byui.edu>
2023-09-18 12:16:26 -0600

    prepped, added random

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -3,43 +3,53 @@ class Program
 {
     static void Main(string[] args)
     {
-        int x = 5;
-        int y = 4;
-        string name;
-        int number1;
-        int number2;
-
-        System.Console.WriteLine("What is your name? ");
-
-        name = Console.ReadLine();
-
-        if (name == "John")
-        {
-            System.Console.WriteLine("Great name! ");
-        } 
-        else
-        {
-            System.Console.WriteLine("That's a cool name! ");
-        }
-
-
-        System.Console.WriteLine("Give me a number");
-        number1 = Convert.ToInt32(Console.ReadLine());
-
-        System.Console.WriteLine("Give me another number");
-        number2 = Convert.ToInt32(Console.ReadLine());
-
-        if (number1 > number2)
-        {
-            System.Console.WriteLine("Your first number is greater than the second. ");
-        }
-        else if (number1 < number2)
-        {
-            System.Console.WriteLine("Your second number is greater than the first. ");
-        }
-        else
-        {
-            System.Console.WriteLine("I don't know about this one... ");
-        }
+        
     }
+    
+    
+    
+    
+    // {
+    //     Random rnd = new Random();
+    //     string name;
+    //     int number1;
+    //     int number2;
+
+    //     System.Console.WriteLine("What is your name? ");
+
+    //     name = Console.ReadLine();
+
+    //     if (name == "John")
+    //     {
+    //         System.Console.WriteLine("Great name! ");
+    //     } 
+    //     else
+    //     {
+    //         System.Console.WriteLine("That's a cool name! ");
+    //     }
+
+
+    //     System.Console.WriteLine("Give me a number");
+    //     number1 = Convert.ToInt32(Console.ReadLine());
+
+    //     System.Console.WriteLine("Give me another number");
+    //     number2 = Convert.ToInt32(Console.ReadLine());
+
+    //     if (number1 > number2)
+    //     {
+    //         System.Console.WriteLine("Your first number is greater than the second. ");
+    //     }
+    //     else if (number1 < number2)
+    //     {
+    //         System.Console.WriteLine("Your second number is greater than the first. ");
+    //     }
+    //     else if (number1 == number2)
+    //     {
+    //         System.Console.WriteLine("Your first number is equal to the second number. ");
+    //     }
+    //     else
+    //     {
+    //         System.Console.WriteLine("I don't know about this one... ");
+    //     }
+    // }
 }
