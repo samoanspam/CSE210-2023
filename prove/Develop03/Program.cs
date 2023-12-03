9968b921aa95479150a9551a6ac0b0fc17444d8d
samoanspam <fateia.patane@gmail.com>
2023-12-03 16:01:13 -0700

    doodoo

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,23 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
+        Scripture scripture1 = new Scripture("For God so loved the world, that he gave his only Son, that whoever believes in him should not perish but have eternal life.", new Reference("John", 3, 16));
+        Scripture scripture2 = new Scripture("In the beginning was the Word, and the Word was with God, and the Word was God.", new Reference("John", 1, 1));
+        Scripture scripture3 = new Scripture("Jesus answered, Verily, verily, I say unto thee, Except a man be born of water and of the Spirit, he cannot enter into the kingdom of God.", new Reference("John", 3, 5));
+        Scripture scripture4 = new Scripture("If ye love me, keep my commandments.", new Reference("John", 14, 15));
 
         List<Scripture> scripturesList = new List<Scripture>();
-        {
-        new Scripture("For God so loved the world, that he gave his only Son, that whoever believes in him should not perish but have eternal life.", new Reference("John", 3, 16));
-        new Scripture("In the beginning was the Word, and the Word was with God, and the Word was God.", new Reference("John", 1, 1));
-        new Scripture("Jesus answered, Verily, verily, I say unto thee, Except a man be born of water and of the Spirit, he cannot enter into the kingdom of God.", new Reference("John", 3, 5));
-        new Scripture("If ye love me, keep my commandments.", new Reference("John", 14, 15));
-        }
         
-/*         scripturesList.Add(scripture1);
+        scripturesList.Add(scripture1);
         scripturesList.Add(scripture2);
         scripturesList.Add(scripture3);
-        scripturesList.Add(scripture4); */
+        scripturesList.Add(scripture4);
 
         Random random = new Random();
-
-        Scripture scripture = scripturesList[random.Next(1, scripturesList.Count())] ;
+        
+        Scripture scripture = scripturesList[random.Next(0, scripturesList.Count())] ;
         
 
         while (true)
