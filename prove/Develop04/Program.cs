d911afdf5b4bdaca050a0235b1e0720edd223dbc
samoanspam <fateia.patane@gmail.com>
2023-12-03 20:39:57 -0700

    added menu and choices with switch case

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,5 +1,44 @@
 using System;
-
 class Program
 {
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("Select an activity: ");
+            Console.WriteLine("Breathing Activity ");
+            Console.WriteLine("Reflection Activity ");
+            Console.WriteLine("Listing Activity ");
+        
+            int choice;
+
+            if (!int.TryParse(Console.ReadLine(), out choice))
+            {
+                System.Console.WriteLine("Invalid input. Please enter a number");
+                continue;
+            }
+
+            switch (choice)
+            {
+                case 1:
+                    BreathingActivity breathingActivity = new BreathingActivity();
+                    breathingActivity.StartActivity();
+                    break;
+                case 2:
+                    ListingActivity listingActivity = new ListingActivity();
+                    listingActivity.StartActivity();
+                    break;
+                case 3:
+                    ReflectionActivity reflectionActivity = new ReflectionActivity();
+                    reflectionActivity.StartActivity();
+                    break;
+                case 4:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    System.Console.WriteLine("Invalid choice. Please select a valid option");
+                    break;
+            }
+        }
+    }
 }
