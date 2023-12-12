f95edc9d8940ddbadf9ac6a8795f5cac31194af6
samoanspam <fateia.patane@gmail.com>
2023-12-12 10:55:17 -0700

    main menu set

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Data;
 
 class Program
 {
     static void Main()
     {
+        //Creating a new user
+        User user = new User();
         
+        while (true)
+        {
+            System.Console.WriteLine("Welcome to the Goal Program!");
+            System.Console.WriteLine();
+            System.Console.WriteLine("Main Menu:");
+            System.Console.WriteLine("1. Create a Goal");
+            System.Console.WriteLine("2. Record an Event");
+            System.Console.WriteLine("3. Display all Goals");
+            System.Console.WriteLine("4. Display user Score");
+            System.Console.WriteLine("5. Exit");
+
+            if (int.TryParse(Console.ReadLine(), out int choice))
+            {
+                switch (choice)
+                {
+                    case 1:
+                        CreateGoal();
+                        break;
+                    case 2:
+                        RecordEvent();
+                        break;
+                    case 3:
+                        DisplayGoals();
+                        break;
+                    case 4:
+                        DisplayScore();
+                        break;
+                    case 5:
+                        System.Console.WriteLine("Thank you for using our program, goodbye!");
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        System.Console.WriteLine("Invalid Choice. Please enter a valid choice between 1-5.");
+                        break;                                                                                                                    
+                }
+            }
+        }
     }
 }
